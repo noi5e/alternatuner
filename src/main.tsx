e55849081967f63fb5d39553636b127f27af0ad3
noi5e <will.gutierrez@gmail.com>
2026-06-07 15:55:57 -0700

    install tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
-import "./index.css";
 import { router } from "./app/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
