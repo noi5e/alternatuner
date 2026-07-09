bb93a08937c99419f6107ecccab03552b6fbd327
noi5e <will.gutierrez@gmail.com>
2026-07-09 14:13:12 -0700

    implement 'attack, decay, sustain, release' audio envelope

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -2,18 +2,25 @@ export function playNote(audioContext: AudioContext, hertz: number) {
   const oscillator = audioContext.createOscillator(); // creates an OscillatorNode which represents a periodic waveform, or constant tone
   const gainNode = audioContext.createGain(); // creates a GainNode, which controls the overall volume of the note
 
-  oscillator.type = "sine";
+  oscillator.type = "triangle";
   oscillator.frequency.setValueAtTime(hertz, audioContext.currentTime);
   oscillator.connect(gainNode);
   gainNode.connect(audioContext.destination);
 
   const now = audioContext.currentTime;
   const duration = 1.5; // seconds
-  const volume = 0.15;
 
-  gainNode.gain.setValueAtTime(0, now);
-  gainNode.gain.linearRampToValueAtTime(volume, now + 0.01);
-  gainNode.gain.setValueAtTime(volume, now + duration - 0.05);
+  const peak = 0.18; // peak volume
+  const sustain = 0.08; // after "peak," volume "decays" to this level, and sustains volume until "duration" ends
+  const release = 0.4; // seconds, time to fade from "sustained" volume to 0
+
+  const attack = 0.02; // seconds, time to reach peak volume
+  const decay = 0.2; // seconds, time to decay from peak to sustain
+
+  gainNode.gain.setValueAtTime(0, now); // start at volume 0
+  gainNode.gain.linearRampToValueAtTime(peak, now + attack); // ramp up to peak volume
+  gainNode.gain.exponentialRampToValueAtTime(sustain, now + attack + decay); // decay to sustain volume
+  gainNode.gain.setValueAtTime(sustain, now + duration - release); // hold sustain volume until it's time to release
   gainNode.gain.linearRampToValueAtTime(0, now + duration);
 
   oscillator.start(now);
